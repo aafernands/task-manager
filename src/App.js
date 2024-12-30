bbe47bc95b0ef3abdec5c139086296f39f087618
Alex Fernandes <alexfernands@outlook.com>
2024-12-30 12:38:50 -0500

    connected App.js and started managing usestate

--- src/App.js
+++ src/App.js
@@ -1,12 +1,36 @@
+import React, { useState } from 'react';
+import TaskForm from './components/TaskForm';
+import TaskList from './components/TaskList';
 import './App.css';
-import Task from './Components/Task/Tasks';
 
 function App() {
+  const [tasks, setTasks] = useState([]);
+
+  const addTask = (task) => {
+    setTasks([
+      ...tasks,
+      { ...task, id: Date.now(), completed: false }
+    ]);
+  };
+
+  const handleDelete = (id) => {
+    setTasks(tasks.filter((task) => task.id !== id));
+  };
+
+  const handleComplete = (id) => {
+    setTasks(
+      tasks.map((task) =>
+        task.id === id ? { ...task, completed: !task.completed } : task
+      )
+    );
+  };
+
   return (
-<div>
-<Task />
-</div>
-   
+    <div className="App">
+      <h1>Task Manager</h1>
+      <TaskForm addTask={addTask} />
+      <TaskList tasks={tasks} handleDelete={handleDelete} handleComplete={handleComplete} />
+    </div>
   );
 }
 
